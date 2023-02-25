abeb478d98fdbf97e32fd6bfa882cebbcf33f129
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-02-25 18:37:04 -0300

    add jest to monorepo

---
+++ jest-setup.ts
@@ -0,0 +1,31 @@
+import "@testing-library/jest-dom/extend-expect";
+import React from "react";
+
+global.React = React;
+// @ts-ignore
+window.ResizeObserver =
+  // @ts-ignore
+  window.ResizeObserver ||
+  jest.fn().mockImplementation(() => ({
+    disconnect: jest.fn(),
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+  }));
+
+class MockPointerEvent extends Event {
+  button: number;
+  ctrlKey: boolean;
+  pointerType: string;
+
+  constructor(type: string, props: PointerEventInit) {
+    super(type, props);
+    this.button = props.button || 0;
+    this.ctrlKey = props.ctrlKey || false;
+    this.pointerType = props.pointerType || "mouse";
+  }
+}
+
+window.PointerEvent = MockPointerEvent as any;
+window.HTMLElement.prototype.scrollIntoView = jest.fn();
+window.HTMLElement.prototype.releasePointerCapture = jest.fn();
+window.HTMLElement.prototype.hasPointerCapture = jest.fn();
---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // roots: ["<rootDir>/packages/**/src"],
+  moduleNameMapper: {},
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
+  clearMocks: true,
+  collectCoverage: true,
+  silent: true,
+  collectCoverageFrom: [
+    "packages/**/src/**",
+    "!packages/**/src/**/styles.ts(x)",
+    "!packages/**/src/**/*stories.ts(x)",
+    "!packages/**/src/**/types.ts(x)",
+    "!packages/**/src/**/values.ts(x)",
+    "!packages/**/src/**/__snapshots__/**/*",
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 90,
+    },
+  },
+};
