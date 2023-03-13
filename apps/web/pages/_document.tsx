fc0f9e356c43562456f25e858350a99b92d26902
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-03-13 10:14:15 -0300

    feat: add button dep to projects

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -13,6 +13,10 @@ export default class Document extends NextDocument {
             id="stitches"
             dangerouslySetInnerHTML={{ __html: getCssText() }}
           />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          ></meta>
         </Head>
         <body>
           <Main />
